afbd1798a67654afdb03fa563329e873e5a70c3c
maximkoo <svazarm@yandex.ru>
2024-03-31 08:14:17 +0300

    Начато упражнение 2.4.1

---
+++ Java_2_4/Java_2_4_2/src/App.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+    public static void main(String[] args) {
+        var users = new ArrayList<>(List.of(
+                new User("Salli", LocalDate.of(1990, 12, 15)),
+                new User("Gawen", LocalDate.of(2002, 10, 23)),
+                new User("Emmalee", LocalDate.of(1992, 9, 16))
+        ));
+
+        var oldestUsers1 = App.getOldest(users);
+        System.out.println(oldestUsers1); // => [Salli 1990-12-15]
+
+        var oldestUsers2 = App.getOldest(users, 2);
+        System.out.println(oldestUsers2); // => [Salli 1990-12-15, Emmalee 1992-9-16]
+    }
+}
